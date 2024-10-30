28691b0ec9943f12cf74303b1bdf0346ecc7c0e1
Sarthak Mishra <msarthak785@gmail.com>
2024-10-30 22:18:29 +0530

    works

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 import { FaSearch } from "react-icons/fa";
 import { IoHome } from "react-icons/io5";
-import { FaDownload } from "react-icons/fa6";
 import { useState } from "react";
+import { toast } from "react-toastify";
 
 import Heading from "../components/Heading";
 import { LoadingSearchButton } from "../components/LoadingSearchButton";
-import { VideoInfo } from "../components/VideoInfo";
 import { ValidUrl } from "../components/ValidUrl";
 
 export interface videoInfoType {
@@ -15,45 +14,45 @@ export interface videoInfoType {
     thumbnail: string,
 }
 
-interface videoFormatType {
-    itag: number,
-    mimeType: string,
-    hasAudio: boolean,
-    hasVideo: boolean,
-    qualityLabel: string,
-    container: string
-}
-
 export const Dashboard = () => {
     const [url, setUrl] = useState<string | null>(null);
     const [isUrlValid, setIsUrlValid] = useState<boolean>(false);
     const [searching, setSearching] = useState<boolean>(false);
-    const [videoInfo, setVideoInfo] = useState<videoInfoType | null>(null);
-    const [videoFormats, setVideoFormats] = useState<videoFormatType[]>([]);
-    const [formatToDownload, setFormatToDownload] = useState<string>("videoandaudio");
+    const [isAudio, setIsAudio] = useState<boolean>(false);
+    const [mediaUrl, setMediaUrl] = useState<string | null>(null);
 
-    const backendURL = "http://localhost:3000/";
+    const backendURL = import.meta.env.VITE_backendURL;
 
     const handleFindVideo = async () => {
         if(url && url.length>0) {
             setSearching(true);
 
-            const response = await fetch(backendURL, {
-                mode: "no-cors",
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(url)
-            }).then(res => res.json());
-            
-            // setIsUrlValid based on the response
-            setIsUrlValid(response.message);
+            const urlData = {
+                "videoUrl": url,
+                "isAudio": isAudio
+            };
 
-            console.log(response);
+            try {
+                const response = await fetch(backendURL, {
+                    mode: "cors",
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(urlData)
+                });
+
+                const blob = await response.blob();
+                const audioBlobUrl = URL.createObjectURL(blob);
+                setMediaUrl(audioBlobUrl);
+                setIsUrlValid(true);
+            }
+            catch(error) {
+                console.log(error);
+                toast.dark("Something went wrong");
+            }
             setSearching(false);
         }
-        
     }
 
     return(
@@ -64,6 +63,14 @@ export const Dashboard = () => {
                 className="p-2 mt-10 m-6 rounded-lg w-[50%] text-gray-800"
                 onChange={(e) => setUrl(e.target.value)}
             />
+            <div className="flex space-x-4 text-xl">
+                <input
+                    className="w-[22px]"
+                    onChange={() => setIsAudio(!isAudio)}
+                    type="checkbox"
+                />
+                <p>Check this to get only Audio</p>
+            </div>
             <div className="flex space-x-4">
                 {
                     searching ? (
@@ -94,34 +101,16 @@ export const Dashboard = () => {
                 isUrlValid &&
                 <ValidUrl />
             }
-            {
-                videoInfo &&
-                <>
-                    <VideoInfo videoInfo={ videoInfo } />
-                    <div className="flex flex-col justify-center space-y-4">
-                        <p className="text-xl">Select format to download</p>
-                        <select className="text-lg text-[#1a3353] p-2 rounded-md" onChange={(e) => setFormatToDownload(e.target.value)}>
-                            <option value={"videoandaudio"}>
-                                Video
-                            </option>
-                            <option value={"audioonly"}>
-                                Audio
-                            </option>
-                            <option value={"videoonly"}>
-                                Video (No Audio)
-                            </option>
-                        </select>
-                        <button
-                            className="text-[#1a3353] bg-[#e6f1ff] hover:bg-[#1a3353] hover:text-[#e6f1ff] text-xl p-2 m-1 border-2 border-[#b1bac9] rounded-md transition-all"
-                        >
-                            <div className="flex flex-row justify-center items-center space-x-1">
-                                <p>Download</p>
-                                <FaDownload />
-                            </div>
-                        </button>
-                    </div>
-                </>
-            }
+            <div>
+                {
+                    mediaUrl &&
+                    (isAudio ? (
+                        <audio controls src={ mediaUrl }></audio>
+                    ):(
+                        <video controls src={ mediaUrl }></video>
+                    ))
+                }
+            </div>
         </div>
     )
 };
--- server/main.py
+++ server/main.py
@@ -1,30 +1,60 @@
-import pytube
-from pytube import YouTube
-from flask import Flask, request, jsonify
+import yt_dlp
+from flask import Flask, request, jsonify, send_file
 from flask_cors import CORS
+import tempfile
 
 app = Flask(__name__)
 CORS(app)
 
 @app.route("/")
 def home():
-    return "Hi from flask backend"
+    return "Hi from freeyoutubedownloader.vercel.app backend"
 
 @app.route("/", methods = ["POST"])
 def getUrl():
-    if request.method == "POST":
-        url_data = request.get_json()
-        url = url_data["url"]
-        print(url)
+    try:
+        if request.method == "POST":
+            data = request.get_json()
+            url = data["videoUrl"]
+            isAudio = data["isAudio"]
 
-        yt = YouTube(url)
-        is_url_valid = False
-        is_url_valid = yt.check_availability()
-        print("IS URL VALID", is_url_valid)
+            try:
+                is_url_valid = True
+            except:
+                is_url_valid = False
+                return jsonify({ "message": "URL Not Valid" })
+        
+        if is_url_valid:
+            temp_dir = tempfile.mkdtemp()
+
+            if isAudio:
+                ytdl_opts = {
+                    "format": "bestaudio/best",
+                    'outtmpl': f'{temp_dir}/%(filename)s.%(ext)s'
+                }
+            else:
+                ytdl_opts = {
+                    'outtmpl': f'{temp_dir}/%(filename)s.%(ext)s'
+                }
+
+            with yt_dlp.YoutubeDL(ytdl_opts) as ydl:
+                try:
+                    info_dict = ydl.extract_info(url, download=True)
+                    file_path = ydl.prepare_filename(info_dict)
+
+                    # Send the audio file to the frontend
+                    return send_file(file_path, as_attachment=False)
+                except Exception as e:
+                    return jsonify({ "message": str(e) })
     
-    return jsonify({ "isUrlValid": is_url_valid })
+    except TypeError:
+        print(TypeError)
+
+    except NameError:
+        print(NameError)
+
+    except Exception as e:
+        print("Error Occured")
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000, host="0.0.0.0")
-    print("Works")
-    print(pytube.__version__)
